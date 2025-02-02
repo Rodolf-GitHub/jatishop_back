395afdad3bc68ad32d6ae1941804fce819c36514
rodolfo <rodolfogroero2@gmail.com>
2025-02-02 10:45:29 -0500

    fixed:added pedidos to documentation

--- api/urls/admin_urls/pedido_admin_urls.py
+++ api/urls/admin_urls/pedido_admin_urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from ...views.admin_views.pedido_admin_views import AdminPedidoViewSet
 
 router = DefaultRouter()
-router.register('pedidos', AdminPedidoViewSet, basename='admin-pedido')
+router.register('pedidos-admin', AdminPedidoViewSet, basename='admin-pedido')
 
 urlpatterns = router.urls
---
+++ api/views/public_views/__init__.py
@@ -0,0 +1,8 @@
+from .pedido_views import PedidoViewSet
+from .categoria import CategoriaViewSet
+from .marketplace import MarketplaceProductoViewSet
+from .producto import ProductoViewSet
+
+
+
+__all__ = ['PedidoViewSet','CategoriaViewSet','MarketplaceProductoViewSet','ProductoViewSet'] 
